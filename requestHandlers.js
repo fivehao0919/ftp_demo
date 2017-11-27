5e57a8b3a7574e0ce6e7ffa12daa5e5413c34c94
wuhao4 <wuhao4@100tal.com>
2017-11-27 15:43:23 +0800

    add

--- requestHandlers.js
+++ requestHandlers.js
@@ -34,7 +34,7 @@ function upload(response, postData) {
 
 function show(response, postData) {
     console.log("Request handler 'show' was called.");
-    fs.readFile("E:/jpg/111.png", "binary", function(error, file) {
+    fs.readFile("/tmp/test.png", "binary", function(error, file) {
         if(error) {
             response.writeHead(500, {"Content-Type": "text/plain"});
             response.write(error + "\n");
